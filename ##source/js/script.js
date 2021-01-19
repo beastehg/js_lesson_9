78ec643c0cc353b587b05de91b0052f4dfc27f85
Dima Yakushev <beastehg@gmail.com>
2021-01-19 21:26:26 +0300

    fix  < 10 better desigion

--- ##source/js/script.js
+++ ##source/js/script.js
@@ -5,20 +5,26 @@ document.querySelector("body").appendChild(h2);
 
 let linkStartTimer = null;
 
-let fixTime = (time) => {
-	if (time.getMinutes() < 10 && time.getSeconds() < 10)
-		return "0" + time.getMinutes() + ":" + "0" + time.getSeconds();
-	else if (time.getMinutes() >= 10 && time.getSeconds() < 10)
-		return time.getMinutes() + ":" + "0" + time.getSeconds();
-	else if (time.getMinutes() <= 10 && time.getSeconds() >= 10)
-		return "0" + time.getMinutes() + ":" + time.getSeconds();
-	else return time.getMinutes() + ":" + time.getSeconds();
-};
+// let fixTime = (time) => {
+// 	if (time.getMinutes() < 10 && time.getSeconds() < 10)
+// 		return "0" + time.getMinutes() + ":" + "0" + time.getSeconds();
+// 	else if (time.getMinutes() >= 10 && time.getSeconds() < 10)
+// 		return time.getMinutes() + ":" + "0" + time.getSeconds();
+// 	else if (time.getMinutes() <= 10 && time.getSeconds() >= 10)
+// 		return "0" + time.getMinutes() + ":" + time.getSeconds();
+// 	else return time.getMinutes() + ":" + time.getSeconds();
+// };
 
 let timer = () => {
 	linkStartTimer = setInterval(() => {
 		time = new Date();
-		h2.innerHTML = `Время: ${time.getHours()}:${fixTime(time)}`;
+		h2.innerHTML = `Время: ${
+			time.getHours() < 10 ? "0" + time.getHours() : time.getHours()
+		}:${
+			time.getMinutes() < 10 ? "0" + time.getMinutes() : time.getMinutes()
+		}:${
+			time.getSeconds() < 10 ? "0" + time.getSeconds() : time.getSeconds()
+		}`;
 	}, 1000);
 };
 
